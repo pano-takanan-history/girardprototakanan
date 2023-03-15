b4844ac78cf49abeb97c017d53d4b8d13e63e557
Frederic Blum <frederic.blum@hu-berlin.de>
2023-03-15 15:38:10 +0100

    update ortho script

--- etc/multiple_profiles.py
+++ etc/multiple_profiles.py
@@ -1,14 +1,18 @@
-from csvw.dsv import UnicodeDictReader
+"""
+Module creates individual ortho-profiles for a set of languages.
+"""
+
 from collections import defaultdict
-from clldutils.text import strip_brackets
 from unicodedata import normalize
+from csvw.dsv import UnicodeDictReader
+
 
 with UnicodeDictReader('cldf/forms.csv') as reader:
     data = [row for row in reader]
 
-with UnicodeDictReader('etc/orthography.tsv', delimiter="\t") as reader:
+with UnicodeDictReader('etc/archive/orthography.tsv', delimiter="\t") as ortho:
     profile = {}
-    for row in reader:
+    for row in ortho:
         profile[normalize('NFC', row['Grapheme'])] = row['IPA']
 
 languages = set([row['Language_ID'] for row in data])
@@ -26,38 +30,15 @@ for row in data:
         char = normalize('NFC', char)
         profiles[row['Language_ID']][char, profile.get(char, '?'+char)] += 1
 
-    if [x for x in '˩˨˧˦˥' if x in row['Value']]:
-        tone = False
-        out = []
-        for char in strip_brackets(row['Form']).split(' '):
-            # print(char)
-            char = normalize('NFC', char)
-            if [x for x in '˩˨˧˦˥' if x in char]:
-                if tone:
-                    out[-1] += char
-                else:
-                    out += [char]
-                tone = True
-            else:
-                tone = False
-                try:
-                    out[-1] = char+out[-1]
-                except IndexError:
-                    out += [char]
-        lexemes[row['Value']] = ''.join(out)
-
 for language in languages:
-    with open('etc/orthography/'+language+'.tsv', 'w') as f:
+    with open('etc/orthography/'+language+'.tsv', 'w', encoding="utf8") as f:
         f.write('Grapheme\tIPA\tFrequency\n')
         for (char, ipa), freq in profiles[language].items():
-            f.write('{0}\t{1}\t{2}\n'.format(char, ipa, freq))
+            f.write(f'{char}\t{ipa}\t{freq}\n')
             if ipa.startswith('?'):
                 errors[char] = ipa[1:]
 
-with open('etc/lexemes2.tsv', 'w') as f:
-    for a, b in lexemes.items():
-        f.write(a+'\t'+b+'\tswap tones\n')
-
-with open('etc/addons.tsv', 'w') as f:
-    for a, b in errors.items():
-        f.write(a+'\t'+b+'\n')
+if len(errors) > 1:
+    with open('etc/addons.tsv', 'w', encoding="utf8") as f:
+        for a, b in errors.items():
+            f.write(a+'\t'+b+'\n')
